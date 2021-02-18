6ed7262a6550d0f310c8465db7c8bdef60c7d173
Binghui Liu <bertieliu52@gmail.com>
2021-02-18 22:28:26 +0200

    509

---
+++ 401-600/509-fibonacci-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int fib(int n) {
+        if (n < 1) return 0;
+        if (n == 1 || n == 2) return 1;
+        int prev = 1, curr = 1;
+        for (int i = 3; i <= n; i++) {
+            int sum = prev + curr;
+            prev = curr;
+            curr = sum;
+        }
+
+        return sum;
+    }
+}
