872a93250c8d706f0c041fd61beddb3db07b0d21
Binghui Liu <bertieliu52@gmail.com>
2021-03-01 22:10:16 +0200

    :92:

---
+++ 1-200/92-reverse-linked-list-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == right)
+            return head;
+
+        if (left == 1) {
+            return reverseN(head, right);
+        } else {
+            head.next = reverseBetween(head.next, left - 1, right - 1);
+            return head;
+        }
+    }
+
+    ListNode successor = null;
+    public ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+}
