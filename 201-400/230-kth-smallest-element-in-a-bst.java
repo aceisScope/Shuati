317955c836848d4f7698293b4edc0bf233c336b0
Binghui Liu <bertieliu52@gmail.com>
2021-03-07 19:38:38 +0200

    230

---
+++ 201-400/230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        traverse(root, k);
+        return res;
+    }
+
+    int res = 0;
+    int rank = 0;
+
+    void traverse(TreeNode root, int k) {
+        if (root == null)
+            return;
+
+        traverse(root.left, k);
+        rank++;
+        if (rank == k) {
+            res = root.val;
+            return;
+        }
+        traverse(root.right, k);
+    }
+}
