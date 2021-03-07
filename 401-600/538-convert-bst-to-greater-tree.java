9652e079e3f0ab7347e86ba1a8cccede4d5c182c
Binghui Liu <bertieliu52@gmail.com>
2021-03-07 19:43:45 +0200

    538

---
+++ 401-600/538-convert-bst-to-greater-tree.java
@@ -0,0 +1,18 @@
+class Solution {
+    // 相当于将BST降序打印
+    public TreeNode convertBST(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    int sum = 0;
+    void traverse(TreeNode root) {
+        if (root == null)
+            return;
+        
+        traverse(root.right);
+        sum += root.val;
+        root.val = sum;
+        traverse(root.lef);
+    }
+}
