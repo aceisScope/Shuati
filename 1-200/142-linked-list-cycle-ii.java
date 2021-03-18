cc3f17089858e08431b162375270d845678e16e2
Binghui Liu <bertieliu52@gmail.com>
2021-03-18 20:47:15 +0200

    142

---
+++ 1-200/142-linked-list-cycle-ii.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) break;
+        }
+
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
