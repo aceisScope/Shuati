19f9bc729b2f3759de166e5a0a8b8826382575f0
Binghui Liu <bertieliu52@gmail.com>
2021-03-24 20:20:26 +0200

    1

---
+++ 1-200/1-two-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], i);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            int other = target - nums[i];
+            if (map.containsKey(other) && map.get(other) != i) {
+                return new int[]{i, map.get(other)};
+            }
+        }
+        
+        return new int[]{-1, -1};
+    }
+}
